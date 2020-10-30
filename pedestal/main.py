199b1949976c1c9bb78502b88bf2e93034691d55
Richard A Hendricks <richardhendricks@pobox.com>
2020-10-29 21:10:05 -0500

    Keys working

--- pedestal/main.py
+++ pedestal/main.py
@@ -6,7 +6,7 @@
 # - One key will control how the lights cycle - multicolor, single color, etc
 # - One key will control how bright the lights pulse, from minimum, 25, 50, 75, maximum
 # - Red LED will notify when keypress is detected
-# - Pause rate is determined by chip temperature - 25s <60F, 15s at 70F, 5s at 80F
+# - Pause rate is determined by chip temperature - 25s <70F, 15s at 80F, 5s at 90F
 
 from digitalio import DigitalInOut, Direction, Pull
 from touchio import TouchIn
@@ -16,15 +16,18 @@ import board
 import time
 
 pulseTypes = {
-  "MULTICOLOR":0,
-  "RED":1,
-  "GREEN":2,
-  "BLUE":3,
-  "WHITE":4
+  0:"MULTICOLOR",
+  1:"BLUE",
+  2:"", # g+b
+  3:"GREEN",
+  4:"", # r+g
+  5:"WHITE", # r+g+b
+  6:"", # r+b 
+  7:"RED",
   }
 
 cycle = 0 # Current location in the cycle
-pulse = pulseTypes["MULTICOLOR"] # Current pulse choice.
+pulse = 0 # Current pulse choice.
 delay = 15
 
 # Built-in dotStar
@@ -47,7 +50,7 @@ def updateDelay():
   print("Temp: %0.1f" % temp)
   
   # Calculate our delay function
-  newDelay = (70-temp) + 15
+  newDelay = (80-temp) + 15
   if newDelay < 5:
    return 5
   if newDelay > 25:
@@ -58,5 +61,24 @@ while True:
   # Check temperature and update pulse rate
   delay = updateDelay()
   print("Delay: %0f" % delay)
+  # Execute pulse program
+
   # Rest
-  time.sleep(delay)
+  delayCount = delay - 1
+  # This way there is a 1.5s delay before checking keys again
+  time.sleep(1)
+  while(delayCount > 0):
+    time.sleep(.5)
+    if changePulse.value:
+      pulse = (pulse + 1) % (len(pulseTypes) - 1)
+      print("New pulse: " + pulseTypes[pulse])
+      # Exit the delay while loop and start the next pulse immediately
+      break  
+    if changeBrightness.value:
+      bright = bright + .25
+      if bright > 1.01:
+        bright = 0.01
+      print("Brightness: " + str(bright))
+      # Exit the delay while loop and start the next pulse immediately
+      break
+    delayCount=delayCount-.5
