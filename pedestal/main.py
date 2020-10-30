166e1fadb347cf81ff58247b412bea0c8ac59d30
Richard A Hendricks <richardhendricks@pobox.com>
2020-10-29 22:05:19 -0500

    Color choice and brightness working

--- pedestal/main.py
+++ pedestal/main.py
@@ -18,21 +18,39 @@ import time
 pulseTypes = {
   0:"MULTICOLOR",
   1:"BLUE",
-  2:"", # g+b
+  2:"AQUA", # g+b
   3:"GREEN",
-  4:"", # r+g
+  4:"YELLOW", # r+g
   5:"WHITE", # r+g+b
-  6:"", # r+b 
+  6:"MAGENTA", # r+b 
   7:"RED",
   }
 
+pulseColors = {
+  0:[0x00,0x00,0x00], # BLACK - Because I am a doofus, I am doing this using Grey code
+  1:[0x00,0x00,0xff], # BLUE
+  2:[0x00,0xff,0xff], # AQUA
+  3:[0x00,0xff,0x00], # GREEN
+  4:[0xff,0xff,0x00], # YELLOW
+  5:[0xff,0xff,0xff], # WHITE
+  6:[0xff,0x00,0xff], # MAGENTA
+  7:[0xff,0x00,0x00]  # RED
+  }
 cycle = 0 # Current location in the cycle
 pulse = 0 # Current pulse choice.
 delay = 15
 
 # Built-in dotStar
-bright = .01
-dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=bright)
+bright = 0
+
+brightNess = {
+  0:0.017,
+  1:0.078,
+  2:0.36,
+  3:1.00
+  }
+
+dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=1)
 
 # Built-in red LED
 led = DigitalInOut(board.D13)
@@ -57,12 +75,27 @@ def updateDelay():
    return 25
   return newDelay
 
+def pulseLED(color):
+  global cycle
+  if (color == 0): # Multicolor, so cycle through all colors
+    color = cycle
+    cycle = (cycle + 1) % (len(pulseTypes))
+    if color != 0:
+      print("Pulsing Multicolor: " + pulseTypes[color])
+    else:
+      print("Pulsing Multicolor: BLACK")
+  else:
+    print("Pulsing: " + pulseTypes[pulse])
+  dot[0]=int(pulseColors[color][0]*brightNess[bright]), int(pulseColors[color][1]*brightNess[bright]),int(pulseColors[color][2]*brightNess[bright])
+  dot.show()
+  return
+    
 while True:
   # Check temperature and update pulse rate
   delay = updateDelay()
   print("Delay: %0f" % delay)
   # Execute pulse program
-
+  pulseLED(pulse)
   # Rest
   delayCount = delay - 1
   # This way there is a 1.5s delay before checking keys again
@@ -70,15 +103,13 @@ while True:
   while(delayCount > 0):
     time.sleep(.5)
     if changePulse.value:
-      pulse = (pulse + 1) % (len(pulseTypes) - 1)
+      pulse = (pulse + 1) % (len(pulseTypes))
       print("New pulse: " + pulseTypes[pulse])
       # Exit the delay while loop and start the next pulse immediately
       break  
     if changeBrightness.value:
-      bright = bright + .25
-      if bright > 1.01:
-        bright = 0.01
-      print("Brightness: " + str(bright))
+      bright = (bright + 1) %(len(brightNess))
+      print("Brightness: " + str(brightNess[bright]))
       # Exit the delay while loop and start the next pulse immediately
       break
     delayCount=delayCount-.5
